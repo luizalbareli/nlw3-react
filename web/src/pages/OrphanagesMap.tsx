a8ad9e79abec2472e388879dfb6a0f11408dffac
Luiz V. Albareli <lvalbareli@gmail.com>
2021-05-01 15:10:10 -0300

    First commit

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,91 @@
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import Leaflet, { popup } from 'leaflet';
+
+import mapMarkerImg from '../images/map-marker.svg';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import '../styles/pages/orphanages-map.css';
+import api from '../services/api';
+
+interface Orphanage{
+    id: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+}
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58,68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
+
+function OrphanagesMap() {
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+    useEffect(() => {
+        api.get('orphanages').then(response => {
+            setOrphanages(response.data);
+        })
+    }, []);
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy" />
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>São josé do Rio Preto</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Map
+                center={[-20.8135947, -49.3891800]}
+                zoom={12}
+                style={
+                    {
+                        width: '100%',
+                        height: '100%'
+                    }
+                }
+            >
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                <TileLayer
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+
+                {orphanages.map(orphanage => {
+                    return (
+                        <Marker
+                            icon={mapIcon}
+                            position={[orphanage.latitude, orphanage.longitude]}
+                            key={orphanage.id}
+                        >
+                            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                                {orphanage.name}
+                                <Link to={`orphanages/${orphanage.id}`}>
+                                    <FiArrowRight size={20} color="#FFF" />
+                                </Link>
+                            </Popup>
+                        </Marker>
+                    )
+                })}
+            </Map>
+
+            <Link to="/orphanages/create" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
+    )
+}
+
+export default OrphanagesMap;
